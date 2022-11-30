e09f9c0ec55796c93a25851191c244ae7d03c71e
alexfandomers <71307225+alexfandomers@users.noreply.github.com>
2022-11-30 16:43:29 -0300

    ArraysBeguinners

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿namespace Arrays
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var numbers = new[] {3,7,9,2,14,6};
+
+            //Length
+            Console.WriteLine("Length: " + numbers.Length);
+
+            //IndexOf()
+            var index = Array.IndexOf(numbers, 9);
+            Console.WriteLine("Index of 9: " + index);
+
+            //Clear
+            Array.Clear(numbers, 0,2);
+            Console.WriteLine("Effect of clear()");
+            foreach(var n in numbers)
+            Console.WriteLine(n);
+
+            //Copy
+            int[] another = new int[3];
+            Array.Copy(numbers, another, 3);
+            Console.WriteLine("Effect of copy()");
+            foreach(var n in another)
+            Console.WriteLine(n);
+
+            //Sort
+            Array.Sort(another);
+            Console.WriteLine("Effect of Srt()");
+            foreach(var n in another)
+            Console.WriteLine(n);
+        }
+    }
+}
